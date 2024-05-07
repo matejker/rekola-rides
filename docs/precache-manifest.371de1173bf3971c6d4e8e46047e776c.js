569e89d8eac27b3f983c47eb5c77bd71ee1c4c92
Matej Kerekrety <matejkerekrety@datavant.com>
2024-05-07 17:12:43 +0100

    build

---
+++ docs/precache-manifest.371de1173bf3971c6d4e8e46047e776c.js
@@ -0,0 +1,322 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543116afe3aad4da866000eba6f623f4",
+    "url": "/rekola-rides/index.html"
+  },
+  {
+    "revision": "b3a979fe4041f82c59c9",
+    "url": "/rekola-rides/static/css/2.f8ca1070.chunk.css"
+  },
+  {
+    "revision": "2354aee7fb8593447f85",
+    "url": "/rekola-rides/static/css/main.e2035e4c.chunk.css"
+  },
+  {
+    "revision": "b3a979fe4041f82c59c9",
+    "url": "/rekola-rides/static/js/2.1e623458.chunk.js"
+  },
+  {
+    "revision": "bd7d9cb8db1363c5c2db28715455bfaf",
+    "url": "/rekola-rides/static/js/2.1e623458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2354aee7fb8593447f85",
+    "url": "/rekola-rides/static/js/main.75646fbc.chunk.js"
+  },
+  {
+    "revision": "15d2e63db19c968754de",
+    "url": "/rekola-rides/static/js/runtime-main.c89433b3.js"
+  },
+  {
+    "revision": "4b7f6c91940688d795b2b5edcb4880c6",
+    "url": "/rekola-rides/static/media/104_clustered_stations.4b7f6c91.png"
+  },
+  {
+    "revision": "ea024baf4c27d09b10e5ff4931d427aa",
+    "url": "/rekola-rides/static/media/104_geo_clustered_stations.ea024baf.png"
+  },
+  {
+    "revision": "3d8245dcb4489694a6a263b05c1cca01",
+    "url": "/rekola-rides/static/media/KaTeX_AMS-Regular.3d8245dc.woff2"
+  },
+  {
+    "revision": "ac1d46d953d403677171697581a284d2",
+    "url": "/rekola-rides/static/media/KaTeX_AMS-Regular.ac1d46d9.woff"
+  },
+  {
+    "revision": "c67be87adba7d31c013be127b936233d",
+    "url": "/rekola-rides/static/media/KaTeX_AMS-Regular.c67be87a.ttf"
+  },
+  {
+    "revision": "3f61a84d76e80396489d32bc9dd8d444",
+    "url": "/rekola-rides/static/media/KaTeX_Caligraphic-Bold.3f61a84d.ttf"
+  },
+  {
+    "revision": "445f96a387df0d13ded71f27c608516d",
+    "url": "/rekola-rides/static/media/KaTeX_Caligraphic-Bold.445f96a3.woff"
+  },
+  {
+    "revision": "970d3e76493b82fccf21ad5888ddee77",
+    "url": "/rekola-rides/static/media/KaTeX_Caligraphic-Bold.970d3e76.woff2"
+  },
+  {
+    "revision": "0ef0f2e356a2e1c457b6585d34edae77",
+    "url": "/rekola-rides/static/media/KaTeX_Caligraphic-Regular.0ef0f2e3.woff2"
+  },
+  {
+    "revision": "74f6918c7d2b768ffd32048102bc0172",
+    "url": "/rekola-rides/static/media/KaTeX_Caligraphic-Regular.74f6918c.woff"
+  },
+  {
+    "revision": "c3bc8fcec0e85a50cabf71e4e8074991",
+    "url": "/rekola-rides/static/media/KaTeX_Caligraphic-Regular.c3bc8fce.ttf"
+  },
+  {
+    "revision": "1aca7ef7f976fb54a207ffc8aa180e38",
+    "url": "/rekola-rides/static/media/KaTeX_Fraktur-Bold.1aca7ef7.woff"
+  },
+  {
+    "revision": "950649ba5e5cfd37cdad74095411d350",
+    "url": "/rekola-rides/static/media/KaTeX_Fraktur-Bold.950649ba.woff2"
+  },
+  {
+    "revision": "e11e6bf02fc41279a540d3655abf3b07",
+    "url": "/rekola-rides/static/media/KaTeX_Fraktur-Bold.e11e6bf0.ttf"
+  },
+  {
+    "revision": "135ccd74931753318f6f52f3fce19018",
+    "url": "/rekola-rides/static/media/KaTeX_Fraktur-Regular.135ccd74.woff2"
+  },
+  {
+    "revision": "a9509497466d16e6b7265a223ea39093",
+    "url": "/rekola-rides/static/media/KaTeX_Fraktur-Regular.a9509497.ttf"
+  },
+  {
+    "revision": "c5b430bfcb3e5423b77401afcdb69b66",
+    "url": "/rekola-rides/static/media/KaTeX_Fraktur-Regular.c5b430bf.woff"
+  },
+  {
+    "revision": "0ba875a1d548ebfdd00c7594022e01b7",
+    "url": "/rekola-rides/static/media/KaTeX_Main-Bold.0ba875a1.woff"
+  },
+  {
+    "revision": "aaaa1b761b7cc9d5e10b85f30731b878",
+    "url": "/rekola-rides/static/media/KaTeX_Main-Bold.aaaa1b76.ttf"
+  },
+  {
+    "revision": "c5b67fb3f6bdaa7f2dd75e5bc8d929eb",
+    "url": "/rekola-rides/static/media/KaTeX_Main-Bold.c5b67fb3.woff2"
+  },
+  {
+    "revision": "0719833c23aeba83786a29e84a165875",
+    "url": "/rekola-rides/static/media/KaTeX_Main-BoldItalic.0719833c.woff2"
+  },
+  {
+    "revision": "5aeca883bc41b522fdf1409ee912b51c",
+    "url": "/rekola-rides/static/media/KaTeX_Main-BoldItalic.5aeca883.woff"
+  },
+  {
+    "revision": "bdbadb27e05fb8e51da903db59f90d77",
+    "url": "/rekola-rides/static/media/KaTeX_Main-BoldItalic.bdbadb27.ttf"
+  },
+  {
+    "revision": "2155d7897af94033a6b6ed44133b867e",
+    "url": "/rekola-rides/static/media/KaTeX_Main-Italic.2155d789.ttf"
+  },
+  {
+    "revision": "6bf616283a81e40e4ac755883862b472",
+    "url": "/rekola-rides/static/media/KaTeX_Main-Italic.6bf61628.woff2"
+  },
+  {
+    "revision": "bb7c45db8908c8fb88a293895a64e018",
+    "url": "/rekola-rides/static/media/KaTeX_Main-Italic.bb7c45db.woff"
+  },
+  {
+    "revision": "1fd21713706a2b70960faf21b1fa4e26",
+    "url": "/rekola-rides/static/media/KaTeX_Main-Regular.1fd21713.woff"
+  },
+  {
+    "revision": "29b27903a08a71f8171be21ba413d4dd",
+    "url": "/rekola-rides/static/media/KaTeX_Main-Regular.29b27903.woff2"
+  },
+  {
+    "revision": "7b8d1fa0668e317c4a2d34399e74af25",
+    "url": "/rekola-rides/static/media/KaTeX_Main-Regular.7b8d1fa0.ttf"
+  },
+  {
+    "revision": "10740d7488d690a743339650103f1cb3",
+    "url": "/rekola-rides/static/media/KaTeX_Math-Italic.10740d74.ttf"
+  },
+  {
+    "revision": "7a31741a44e58952cb4b8a763c206fcd",
+    "url": "/rekola-rides/static/media/KaTeX_Math-Italic.7a31741a.woff2"
+  },
+  {
+    "revision": "8eb56b3ff5b141cd3732a24e65c2b339",
+    "url": "/rekola-rides/static/media/KaTeX_Math-Italic.8eb56b3f.woff"
+  },
+  {
+    "revision": "84299b016163b0ae5c776604aa1cb726",
+    "url": "/rekola-rides/static/media/KaTeX_SansSerif-Bold.84299b01.woff"
+  },
+  {
+    "revision": "9123667cef81279761df5cd62ec8c46e",
+    "url": "/rekola-rides/static/media/KaTeX_SansSerif-Bold.9123667c.ttf"
+  },
+  {
+    "revision": "e38d0f6844b9520510715fd989bdd5bd",
+    "url": "/rekola-rides/static/media/KaTeX_SansSerif-Bold.e38d0f68.woff2"
+  },
+  {
+    "revision": "1057b70228cb6936e7714e7817db6ce2",
+    "url": "/rekola-rides/static/media/KaTeX_SansSerif-Italic.1057b702.woff2"
+  },
+  {
+    "revision": "95c5a3d43efbcb8c14184d00a7c5eed4",
+    "url": "/rekola-rides/static/media/KaTeX_SansSerif-Italic.95c5a3d4.woff"
+  },
+  {
+    "revision": "f9c440abdd25c5a954d855516fed39f9",
+    "url": "/rekola-rides/static/media/KaTeX_SansSerif-Italic.f9c440ab.ttf"
+  },
+  {
+    "revision": "2eae604dbbc4b2c0ceede62c61c9e8df",
+    "url": "/rekola-rides/static/media/KaTeX_SansSerif-Regular.2eae604d.ttf"
+  },
+  {
+    "revision": "35bda414c6c2f75940b086de18f42e72",
+    "url": "/rekola-rides/static/media/KaTeX_SansSerif-Regular.35bda414.woff"
+  },
+  {
+    "revision": "f9e06d59e70de05c5f0b7f7f65c14565",
+    "url": "/rekola-rides/static/media/KaTeX_SansSerif-Regular.f9e06d59.woff2"
+  },
+  {
+    "revision": "60febfa114c5e32f0ce73050476aa39f",
+    "url": "/rekola-rides/static/media/KaTeX_Script-Regular.60febfa1.woff"
+  },
+  {
+    "revision": "ae1fad1f4d1c227c9d567da8ea9f988c",
+    "url": "/rekola-rides/static/media/KaTeX_Script-Regular.ae1fad1f.woff2"
+  },
+  {
+    "revision": "e9169ca7b32608b6235ec9ffff742a71",
+    "url": "/rekola-rides/static/media/KaTeX_Script-Regular.e9169ca7.ttf"
+  },
+  {
+    "revision": "1d6cf6b89f694dc76e13ecc4e8214dd2",
+    "url": "/rekola-rides/static/media/KaTeX_Size1-Regular.1d6cf6b8.woff"
+  },
+  {
+    "revision": "f2e296ef7cef0f6f16912e5d171929b2",
+    "url": "/rekola-rides/static/media/KaTeX_Size1-Regular.f2e296ef.ttf"
+  },
+  {
+    "revision": "fda0824e8cfb676664ad063c367cfce3",
+    "url": "/rekola-rides/static/media/KaTeX_Size1-Regular.fda0824e.woff2"
+  },
+  {
+    "revision": "0767ede65042583bcc06f09055f4d7ca",
+    "url": "/rekola-rides/static/media/KaTeX_Size2-Regular.0767ede6.ttf"
+  },
+  {
+    "revision": "a7a0eb93afb696cb9b4efb87e6cd45a3",
+    "url": "/rekola-rides/static/media/KaTeX_Size2-Regular.a7a0eb93.woff2"
+  },
+  {
+    "revision": "f48e83b7bcaa68a86a78e8edfee1a04e",
+    "url": "/rekola-rides/static/media/KaTeX_Size2-Regular.f48e83b7.woff"
+  },
+  {
+    "revision": "0387ab387ace32f15b43c3a4a39f187f",
+    "url": "/rekola-rides/static/media/KaTeX_Size3-Regular.0387ab38.woff"
+  },
+  {
+    "revision": "397bd6872be7adff4f000c49fa85a9c3",
+    "url": "/rekola-rides/static/media/KaTeX_Size3-Regular.397bd687.woff2"
+  },
+  {
+    "revision": "882f723427a0986846fe786bd8d6f6c7",
+    "url": "/rekola-rides/static/media/KaTeX_Size3-Regular.882f7234.ttf"
+  },
+  {
+    "revision": "27901d5ce93d8971c416d9123fedb911",
+    "url": "/rekola-rides/static/media/KaTeX_Size4-Regular.27901d5c.ttf"
+  },
+  {
+    "revision": "3c14ed11dd8eea981c93bf283193856d",
+    "url": "/rekola-rides/static/media/KaTeX_Size4-Regular.3c14ed11.woff2"
+  },
+  {
+    "revision": "726db3bd435c8a6e45e44741dae67263",
+    "url": "/rekola-rides/static/media/KaTeX_Size4-Regular.726db3bd.woff"
+  },
+  {
+    "revision": "6b9645af6e119fd5d85f24e21044ed03",
+    "url": "/rekola-rides/static/media/KaTeX_Typewriter-Regular.6b9645af.ttf"
+  },
+  {
+    "revision": "ba01f72a8db89265149f3f712dc7f9a3",
+    "url": "/rekola-rides/static/media/KaTeX_Typewriter-Regular.ba01f72a.woff"
+  },
+  {
+    "revision": "f43e86963e9cfda37bb2e958ddedcaad",
+    "url": "/rekola-rides/static/media/KaTeX_Typewriter-Regular.f43e8696.woff2"
+  },
+  {
+    "revision": "a6439ed9613bcc21f97dfdfa0ce680c2",
+    "url": "/rekola-rides/static/media/all_rides_heatmap.a6439ed9.png"
+  },
+  {
+    "revision": "e37fb48e71d13f3c057e67243f767d2e",
+    "url": "/rekola-rides/static/media/all_stations.e37fb48e.png"
+  },
+  {
+    "revision": "8ffcb132da81fe558639489e3c933b91",
+    "url": "/rekola-rides/static/media/drive_rides_heatmap.8ffcb132.png"
+  },
+  {
+    "revision": "4abeffdabf974a15c2cc8b6dd85e762d",
+    "url": "/rekola-rides/static/media/endpoint_heatmap2.4abeffda.png"
+  },
+  {
+    "revision": "aa9109e429e6c279c5efadc98d4ab62c",
+    "url": "/rekola-rides/static/media/ride_duration_frequency.aa9109e4.png"
+  },
+  {
+    "revision": "62aac75d454fc2f431c891bfb14ec3d5",
+    "url": "/rekola-rides/static/media/ride_length_frequency.62aac75d.png"
+  },
+  {
+    "revision": "1c454fc0f38d4e154447b3ffe5c83476",
+    "url": "/rekola-rides/static/media/rides_heatmap.1c454fc0.png"
+  },
+  {
+    "revision": "87c1620a1d532d8a36d94c40043d99d4",
+    "url": "/rekola-rides/static/media/rides_prediction.87c1620a.png"
+  },
+  {
+    "revision": "2bd14073a9be3732553f44a9b032e671",
+    "url": "/rekola-rides/static/media/rides_weekends.2bd14073.png"
+  },
+  {
+    "revision": "330b78cc88ef9570db2363e72714c3e8",
+    "url": "/rekola-rides/static/media/rides_weekends_proportions.330b78cc.png"
+  },
+  {
+    "revision": "c5d26a329a2d9c33ee3f3ddec116d5fe",
+    "url": "/rekola-rides/static/media/rides_workdays.c5d26a32.png"
+  },
+  {
+    "revision": "2b6722315dcb126dfd3165532528948c",
+    "url": "/rekola-rides/static/media/rides_workdays_proportions.2b672231.png"
+  },
+  {
+    "revision": "fd5728f2cf777b06b966d05c0c823dc9",
+    "url": "/rekola-rides/static/media/spritesheet.fd5728f2.svg"
+  },
+  {
+    "revision": "3ad08865918ef81bee738f513b2cf367",
+    "url": "/rekola-rides/static/media/weather_vs_rides.3ad08865.png"
+  }
+]);
